70cf84bd6aaf18d38478df3989135215a34b3fba
Ajeet <ajeetpal101999@gmail.com>
2023-04-05 03:55:19 +0530

    [Ajeet] Interfaces, Constructor and Access Modifiers.

---
+++ Interface_Access_Constructor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Interface_Access_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  1. Constructor  2. Interfaces  3. AccessModifiers  *****");
+        }
+    }
+}
