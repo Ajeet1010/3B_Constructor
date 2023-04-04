ce1ec5fe39492e8a96a2b9009d951e664a78bf3c
Ajeet <ajeetpal101999@gmail.com>
2023-04-05 04:00:34 +0530

    [Ajeet] Constructor.

---
+++ Interface_Access_Constructor/Constructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Access_Constructor
+{
+    // UC1- Constructor
+    internal class Constructor
+    {
+        // fields or members.
+        public int ID;
+        public string Name;
+
+        //1. Empty constructor
+        public Constructor()
+        {
+
+        }
+        public Constructor(string x)
+        {
+            Name = x;
+
+        }
+        public Constructor(int x, string y)
+        {
+            ID = x;
+            Name = y;
+        }
+    }
+}
--- Interface_Access_Constructor/Program.cs
+++ Interface_Access_Constructor/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("*****  1. Constructor  2. Interfaces  3. AccessModifiers  *****");
+            Console.Write("Enter ur choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Constructor");
+                    Constructor constructor = new Constructor(1, "Aman");
+                    Console.WriteLine(constructor.ID + "  " + constructor.Name);
+                    break;
+            }
         }
     }
 }
